49a49a9fd1c77d90b9f26349908d780519f98a0a
George Richardson <georgerichardson@posteo.net>
2022-12-29 15:06:01 +0000

    Add project setup

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
+import setuptools
 from setuptools import setup
-from kohilites import __version__
+# from kohilites import __version__
 
-version = "".join(v for v in __version__ if (v.isnumeric() or v == "."))
+# version = "".join(v for v in __version__ if (v.isnumeric() or v == "."))
+
+with open("README.md", "r") as fr:
+    long_description=fr.read()
 
 
 common_kwargs = dict(
-    version=version,
+    version="0.0.1",
     license="MIT",
-    install_requires=["Mdutils==1.3.1", "SLPP==1.2.3"],
-    long_description=open("README.md").read(),
+    install_requires=["Mdutils==1.4.0", "SLPP==1.2.3"],
     long_description_content_type="text/markdown",
     url="https://github.com/georgerichardson/kohilites",
-    author="Nesta",
+    author="George Richardson",
     author_email="georgerichardson@posteo.net",
     maintainer="georgerichardson",
     maintainer_email="georgerichardson@posteo.net",
@@ -21,10 +24,17 @@ common_kwargs = dict(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    python_requires=">3.6",
+    python_requires=">3.8",
     include_package_data=True,
+    entry_points={
+        "console_scripts": ["kohilites = kohilites.cli:get_highlights",]
+    }
 )
 
-setup(name="kohilites", packages=["kohilites"], **common_kwargs)
+setup(
+    name="kohilites",
+    packages=["kohilites"],
+    **common_kwargs
+)
