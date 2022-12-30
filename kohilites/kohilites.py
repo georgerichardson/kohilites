128e8bb222a8130a10254301cce2ba955d6885f7
George Richardson <georgerichardson@posteo.net>
2022-12-30 14:30:53 +0000

    Handle output attribute in init

--- kohilites/kohilites.py
+++ kohilites/kohilites.py
@@ -7,9 +7,23 @@ from .utils import make_path_if_not_exist
 
 
 class MetaData:
-    def __init__(self, input, output=None):
+    def __init__(self, input, output):
+
+        self.sep = os.path.sep
+
         self.input = input
+
         self.output = output
+        if self.output is None:
+            if os.path.isfile(self.input):
+                print("file")
+                self.output = os.path.join(
+                    *self.input.split(f"{self.sep}")[:-1]
+                )
+            elif os.path.isdir(self.input):
+                print("path")
+                self.output = self.input
+            
 
     def decode_data(self):
         
@@ -18,6 +32,9 @@ class MetaData:
             data = lua.decode(txt.replace("--", "—"))
             if type(data) == dict:
                 return data
+            else:
+                raise ValueError("Could not parse metadata.")
+
 
     def parse_date(self, date):
         return datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
@@ -51,17 +68,9 @@ class MetaData:
         doc_props = self.extract_doc_props(data)
         highlights = self.extract_highlights(data)
 
-        if self.output is None:
-            if os.path.isdir(self.input):
-                fout = self.input
-            elif os.path.isfile(self.input):
-                fout = "/".join(self.input.split("/")[:-1])
-        else:
-            fout = self.output
+        make_path_if_not_exist(self.output)
 
-        make_path_if_not_exist(fout)
-
-        md = MdUtils(file_name=f"{fout}/{doc_props['title']}")
+        md = MdUtils(file_name=os.path.join(self.output, doc_props['title']))
         md.write(f"**{doc_props['title']}**, _{doc_props['authors']}_")
 
         for highlight in highlights:
@@ -69,5 +78,3 @@ class MetaData:
 
         md.create_md_file()
         self.md = md
-
-
