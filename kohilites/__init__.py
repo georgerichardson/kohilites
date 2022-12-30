22f250f8145e20218762d4e5add798fe19bc1fc6
George Richardson <georgerichardson@posteo.net>
2022-12-30 14:29:49 +0000

    Remove commented code

--- kohilites/__init__.py
+++ kohilites/__init__.py
@@ -2,26 +2,3 @@ try:
     from .kohilites import MetaData
 except ModuleNotFoundError:  # Workaround for setup.py
     pass
-
-
-# def path_to_init(_file=__file__, cast_to_str=False):
-#     """Return the path to this file"""
-#     from pathlib import Path
-
-#     path = Path(_file).resolve().parent
-#     return str(path) if cast_to_str else path
-
-
-# def load_current_version():
-#     """Load the current version of this package."""
-#     path_to_version = path_to_init(__file__) / "VERSION"
-#     with open(path_to_version) as f:
-#         v = f.read()
-#     return v
-
-
-# try:
-#     __version__ = load_current_version()
-# except FileNotFoundError:  # Not needed on PyPI, where VERSION has not been bundled
-#     pass
-
