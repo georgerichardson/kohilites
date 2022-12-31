78061da1ce281fcb1edd404695b18940955823f1
George Richardson <georgerichardson@posteo.net>
2022-12-31 14:52:15 +0000

    Add version and description

--- kohilites/__init__.py
+++ kohilites/__init__.py
@@ -1,4 +1,10 @@
-try:
-    from .kohilites import MetaData
-except ModuleNotFoundError:  # Workaround for setup.py
-    pass
+""""A lightweight command line tool to extract and save highlights and notes 
+made on KOReader."
+"""
+
+# try:
+#     from .kohilites import MetaData
+# except ModuleNotFoundError:  # Workaround for setup.py
+#     pass
+
+__version__ = "0.0.1"
